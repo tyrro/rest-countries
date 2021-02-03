71f351fec2b61af1a1af1fc4b458d9526b822f8d
rajibds <rajib.das.shuva@gmail.com>
2021-02-03 15:41:44 +0600

    fix jest :cry:

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
-import { shallow } from "enzyme";
+import { shallow } from 'enzyme';
 
-import App from "./App";
+import App from './App';
 
-describe("App", () => {
+describe('App', () => {
   const wrapper = shallow(<App />);
 
-  test("renders 3 components", () => {
-    expect(wrapper.find("CountryProvider").children.count).toEqual(3);
+  test('renders 3 components', () => {
+    expect(wrapper.find('CountryProvider').children().length).toEqual(3);
   });
 });
